973cf3d73ff5e78d2b7df5b83a832ae9c2f36711
Dimas Muharam <dimas1988@gmail.com>
2025-12-29 13:05:14 +0000

    Create data-static.ts

---
+++ lib/data-static.ts
@@ -0,0 +1,49 @@
+// lib/data-static.ts
+
+export const INDONESIA_CITIES = [
+  "Jakarta Pusat", "Jakarta Selatan", "Jakarta Barat", "Jakarta Timur", "Jakarta Utara",
+  "Bogor", "Depok", "Tangerang", "Tangerang Selatan", "Bekasi",
+  "Bandung", "Surabaya", "Yogyakarta", "Semarang", "Surakarta (Solo)",
+  "Malang", "Denpasar", "Makassar", "Medan", "Palembang",
+  "Banda Aceh", "Padang", "Pekanbaru", "Batam", "Bandar Lampung",
+  "Pontianak", "Banjarmasin", "Samarinda", "Balikpapan", "Manado",
+  "Jayapura", "Ambon", "Mataram", "Kupang"
+  // ... bisa ditambahkan hingga 500+ kota/kab
+].sort();
+
+export const UNIVERSITIES = [
+  "Universitas Indonesia (UI)",
+  "Universitas Gadjah Mada (UGM)",
+  "Institut Teknologi Bandung (ITB)",
+  "Institut Pertanian Bogor (IPB)",
+  "Universitas Brawijaya (UB)",
+  "Universitas Airlangga (UNAIR)",
+  "Universitas Padjadjaran (UNPAD)",
+  "Universitas Diponegoro (UNDIP)",
+  "Institut Teknologi Sepuluh Nopember (ITS)",
+  "Universitas Sebelas Maret (UNS)",
+  "Universitas Hasanuddin (UNHAS)",
+  "UIN Sunan Kalijaga Yogyakarta",
+  "UIN Syarif Hidayatullah Jakarta",
+  "Universitas Negeri Jakarta (UNJ)",
+  "Universitas Pendidikan Indonesia (UPI)",
+  "Universitas Negeri Yogyakarta (UNY)",
+  "Universitas Terbuka (UT)"
+  // ... bisa ditambahkan list lengkap PDDIKTI
+].sort();
+
+export const DISABILITY_TOOLS = [
+  "Screen Reader - NVDA",
+  "Screen Reader - JAWS",
+  "Screen Reader - VoiceOver (Mac/iOS)",
+  "Screen Reader - TalkBack (Android)",
+  "Magnifier (Kaca Pembesar Digital)",
+  "Tongkat Putih (White Cane)",
+  "Kursi Roda Manual",
+  "Kursi Roda Elektrik",
+  "Alat Bantu Dengar (Hearing Aid)",
+  "Bahasa Isyarat (Bisindo)",
+  "Bahasa Isyarat (SIBI)",
+  "Tangan Palsu / Kaki Palsu (Prostetik)",
+  "Tanpa Alat Bantu"
+].sort();
